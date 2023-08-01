6d65da8e2a0af227f272c7b1b26b92d4e6787300
Jose Suniaga [Vauxoo] <josemiguel@vauxoo.com>
2023-08-01 12:58:48 -0600

    [MIG] repair_stock_move: Migration to 16.0

---
+++ repair_stock_move/models/repair_line.py
@@ -0,0 +1,55 @@
+# Copyright (C) 2021 ForgeFlow S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+
+from odoo import api, fields, models
+
+
+class RepairLine(models.Model):
+    _inherit = "repair.line"
+
+    stock_move_ids = fields.One2many(
+        comodel_name="stock.move",
+        inverse_name="repair_line_id",
+    )
+
+    def create_stock_move(self):
+        self.ensure_one()
+        move = self.env["stock.move"].create(
+            {
+                "name": self.repair_id.name,
+                "product_id": self.product_id.id,
+                "product_uom_qty": self.product_uom_qty,
+                "product_uom": self.product_uom.id,
+                "partner_id": self.repair_id.address_id.id,  # TODO: check
+                "location_id": self.location_id.id,
+                "location_dest_id": self.location_dest_id.id,
+                "repair_id": self.repair_id.id,
+                "repair_line_id": self.id,
+                "origin": self.repair_id.name,
+                "company_id": self.company_id.id,
+            }
+        )
+        return move
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        lines = super().create(vals_list)
+        for line in lines:
+            if line and line.repair_id.state == "confirmed":
+                move = line.create_stock_move()
+                line._action_confirm()
+                res.move_id = move
+            if line and line.repair_id.state == "under_repair":
+                move = line.create_stock_move()
+                move._action_confirm()
+                move._action_assign()
+                line.move_id = move
+        return lines
+
+    @api.onchange("product_id")
+    def _onchange_location(self):
+        if self.state == "draft":
+            self.location_id = self.repair_id.location_id
+
+    # TODO: write qty - update stock move.
+    # TODO: default repair location in repair lines.
